9966b2483578172429e3d9adfd8213a39e1417e9
new_user_name <new@email.com>
2017-08-06 16:08:10 +0600

    Commit

--- IslamFAQ/AnswerViewController.swift
+++ IslamFAQ/AnswerViewController.swift
@@ -12,48 +12,29 @@ import EasyPeasy
 class AnswerViewController: UIViewController {
     
     var question = Question(question: "", answer: "")
-    let questionLabel = UILabel()
-    let answerTextView = UITextView()
     
-    func setupViews() {
+    lazy var answerView: AnswerView = {
+        let answerView = AnswerView()
+        return answerView
+    }()
+    
+    func setup() {
         
-        self.view.addSubview(questionLabel)
-        self.view.addSubview(answerTextView)
-        self.view.backgroundColor = UIColor.white
+        self.automaticallyAdjustsScrollViewInsets = false
         
-        questionLabel.text = question.questionTitle
-        questionLabel.backgroundColor = .white
-        questionLabel.textAlignment = .left
-        questionLabel.numberOfLines = 0
-        questionLabel.font = UIFont.boldSystemFont(ofSize: 23)
-        questionLabel.adjustsFontSizeToFitWidth = true
-        
-        answerTextView.text = question.answerTitle
-        answerTextView.textAlignment = .left
-        answerTextView.font = UIFont.risingSunRegular()
-        answerTextView.isEditable = false
-        answerTextView.font = answerTextView.font?.withSize(17)
+        view.addSubview(answerView)
+        answerView.question = question
         
         self.navigationController?.isNavigationBarHidden = false
         self.navigationController?.hidesBarsOnSwipe = true
         self.navigationItem.title = question.questionTitle
         self.navigationController?.navigationBar.backItem?.title = ""
         
-    }
-    
-    func setupLayouts() {
-        questionLabel <- [Size(CGSize(width: self.view.frame.width, height: self.view.frame.height / 7)),
-                          Top().to(self.topLayoutGuide), Bottom().to(answerTextView), Right(20), Left(20)
-        ]
-        
-        answerTextView <- [Size(CGSize(width: self.view.frame.width, height: self.view.frame.height / 1.5)),
-                           Top().to(questionLabel), Bottom(10), Right(20), Left(15)
-        ]
+        answerView <- Edges()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupViews()
-        setupLayouts()
+        setup()
     }
 }
